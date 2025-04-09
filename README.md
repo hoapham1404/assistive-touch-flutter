f05840bc2afc68081a26e3aa3f5335f8a9ef6e14
Phạm Quang Hoà <phamquanghoa1404@gmail.com>
2025-04-09 13:02:29 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# assistive-touch-flutter
+Assistive touch, for android, but Flutter! Cause my phone volume button is broken
---
+++ android/app/src/main/kotlin/com/example/assistive_touch_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.assistive_touch_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
